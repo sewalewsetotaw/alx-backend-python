c2a592ba6185da96125ac707c8ece6c863d23c0a
SewalewS29 <sewalews29@gmail.com>
2025-07-20 06:08:59 +0300

    fixing the checker error

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,12 +2,17 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
+nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(nested_router.urls)),
 ]
